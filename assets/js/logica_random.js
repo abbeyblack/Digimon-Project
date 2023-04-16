1b075244b290bbe5efa054e58c02fbc36a9ebc07
Javiera Paz <javypaz@gmail.com>
2023-04-16 19:59:23 -0400

    addes new html, css and js

---
+++ assets/js/logica_random.js
@@ -0,0 +1,36 @@
+    // Function to get a random integer between min and max (inclusive)
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
+  
+      function getRandomDigimon() {
+        // Fetch the digimon data from the API
+        fetch("https://digimon-api.vercel.app/api/digimon")
+          .then(response => response.json())
+          .then(data => {
+            // Get a random index based on the number of digimon available
+            const randomIndex = getRandomInt(0, data.length - 1);
+            // Get the image URL and name of the random digimon
+            const digimonImage = data[randomIndex].img;
+            const digimonName = data[randomIndex].name;
+  
+            // Display the image on the page
+            document.getElementById("digimonImage").src = digimonImage;
+            document.getElementById("digimonName").textContent = digimonName;
+          })
+          .catch(error => {
+            console.error("Error fetching digimon data:", error);
+          });
+      }
+  
+      function showDigimonName() {
+        // Show/hide the name of the Digimon when the image is clicked
+        const digimonNameElement = document.getElementById("digimonName");
+        if (digimonNameElement.style.display === "block") {
+          digimonNameElement.style.display = "none";
+        } else {
+          digimonNameElement.style.display = "block";
+        }
+      }
